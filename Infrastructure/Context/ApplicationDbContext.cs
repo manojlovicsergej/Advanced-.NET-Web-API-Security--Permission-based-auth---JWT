4a454b93d379fedbe26a9b093fe3872a24be8f60
Manojlovic Sergej <manojlovics@outlook.com>
2023-09-25 19:24:42 +0200

    Add ApplicationDbContext and IdentityUserDbContext interface

---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string, IdentityUserClaim<string>,
+    IdentityUserRole<string>, IdentityUserLogin<string>, IdentityRoleClaim<string>, IdentityUserToken<string>>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
