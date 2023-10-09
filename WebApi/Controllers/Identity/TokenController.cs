f90bdb7bbd717fb987c05752273ef70f1808704e
Sergej Manojlovic <115702767+sergej-kodit@users.noreply.github.com>
2023-10-09 20:28:39 +0200

    GetToken, GetRefreshToken, MyBaseController and TokenController created

---
+++ WebApi/Controllers/Identity/TokenController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Features.Identity.Queries;
+using Common.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Identity;
+
+[Route("api/[controller]")]
+public class TokenController : MyBaseController<TokenController>
+{
+    [HttpPost("get-token")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetTokenAsync([FromBody] TokenRequest tokenRequest)
+    {
+        var response = await MediatorSender.Send(new GetTokenQuery { TokenRequest = tokenRequest });
+        if (response.IsSuccessful)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest(response);
+    }
+    
+    [HttpPost("get-refresh-token")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetTokenAsync([FromBody] RefreshTokenRequest refreshTokenRequest)
+    {
+        var response = await MediatorSender.Send(new GetRefreshTokenQuery() { RefreshTokenRequest = refreshTokenRequest });
+        if (response.IsSuccessful)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest(response);
+    }
+}
---
+++ WebApi/Controllers/MyBaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+public class MyBaseController<T> : ControllerBase
+{
+    private ISender _sender  { get; set; }
+
+    public ISender MediatorSender => _sender ??= HttpContext.RequestServices.GetService<ISender>();
+}
