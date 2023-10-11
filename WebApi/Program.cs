17784f8dfd86300666eb16c3b068034723faaf9a
Sergej Manojlovic <115702767+sergej-kodit@users.noreply.github.com>
2023-10-11 19:45:05 +0200

    Adding JwtAuthentication to Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddIndentitySettings();
+builder.Services.AddJwtAuthentication(builder.Services.GetApplicationSettings(builder.Configuration));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- WebApi/ServiceCollectionExtensions.cs
+++ WebApi/ServiceCollectionExtensions.cs
@@ -1,7 +1,17 @@
-﻿using Infrastructure.Context;
+﻿using System.Net;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+using Application.AppConfigs;
+using Common.Authorization;
+using Common.Responses.Wrappers;
+using Infrastructure.Context;
 using Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using WebApi.Permissions;
 
 namespace WebApi;
@@ -39,5 +49,91 @@ public static class ServiceCollectionExtensions
             .AddDefaultTokenProviders();
 
         return services;
+    }
+
+    internal static IServiceCollection AddJwtAuthentication(this IServiceCollection services, AppConfiguration config)
+    {
+        var key = Encoding.ASCII.GetBytes(config.Secret);
+        services
+            .AddAuthentication(authentication =>
+            {
+                authentication.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authentication.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(bearer =>
+            {
+                bearer.RequireHttpsMetadata = false;
+                bearer.SaveToken = true;
+                bearer.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RoleClaimType = ClaimTypes.Role,
+                    ClockSkew = TimeSpan.Zero
+                };
+
+                bearer.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        if (c.Exception is SecurityTokenExpiredException)
+                        {
+                            c.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            c.Response.ContentType = "application/json";
+                            var result = JsonConvert.SerializeObject(ResponseWrapper.Fail("The Token is expired."));
+                            return c.Response.WriteAsync(result);
+                        }
+                        else
+                        {
+                            c.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            c.Response.ContentType = "application/json";
+                            var result = JsonConvert.SerializeObject(ResponseWrapper.Fail("An unhandled error has occurred."));
+                            return c.Response.WriteAsync(result);
+                        }
+                    },
+                    OnChallenge = context =>
+                    {
+                        context.HandleResponse();
+                        if (!context.Response.HasStarted)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            context.Response.ContentType = "application/json";
+                            var result = JsonConvert.SerializeObject(ResponseWrapper.Fail("You are not Authorized."));
+                            return context.Response.WriteAsync(result);
+                        }
+
+                        return Task.CompletedTask;
+                    },
+                    OnForbidden = context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(ResponseWrapper.Fail("You are not authorized to access this resource."));
+                        return context.Response.WriteAsync(result);
+                    },
+                };
+            });
+
+        services.AddAuthorization(options =>
+        {
+            foreach (var prop in typeof(AppPermissions).GetNestedTypes().SelectMany(c => c.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)))
+            {
+                var propertyValue = prop.GetValue(null);
+                if (propertyValue is not null)
+                {
+                    options.AddPolicy(propertyValue.ToString(), policy => policy.RequireClaim(AppClaim.Permission, propertyValue.ToString()));
+                }
+            }
+        });
+        return services;
+    }
+    
+    internal static AppConfiguration GetApplicationSettings(this IServiceCollection services, IConfiguration configuration)
+    {
+        var applicationSettingsConfiguration = configuration.GetSection(nameof(AppConfiguration));
+        services.Configure<AppConfiguration>(applicationSettingsConfiguration);
+        return applicationSettingsConfiguration.Get<AppConfiguration>();
     }
 }
