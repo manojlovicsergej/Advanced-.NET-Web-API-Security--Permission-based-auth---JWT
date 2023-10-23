0ebdab2f38e1a559ef34dc75f7af10d67fbcd431
Manojlovic Sergej <manojlovics@outlook.com>
2023-10-23 20:20:29 +0200

    Preparing ResponseDto for Implementation of Permissions

---
+++ Common/Responses/Identity/RoleClaimResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Responses.Identity;
+
+public class RoleClaimResponse
+{
+    public RoleResponse Role { get; set; }
+    public List<RoleClaimViewModel> RoleClaims { get; set; }
+}
